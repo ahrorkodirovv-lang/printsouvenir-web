f999687ad97f1d5ee7fa77358e78ce9fa9b80c6d
ahrorkodirovv-lang <ahrorkodirovv@gmail.com>
2025-11-29 20:56:25 +0500

    this my fist project

---
+++ tailwind.config.js
@@ -0,0 +1,92 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './hooks/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        // Light Mode Colors
+        'ivory-white': '#F8F8F7',
+        'champagne-beige': '#E7E2DA',
+        'rich-black': '#0A0A0A',
+        'dark-charcoal': '#232323',
+        'royal-burgundy': '#6A1F1F',
+        'gold-accent': '#C9A66B',
+        'soft-gray': '#F2F2F2',
+        'warm-divider': '#D5D5D5',
+        
+        // Dark Mode Colors
+        'evening-black': '#0c0c48ff',
+        'obsidian-gray': '#151515',
+        'pure-white': '#964646ff',
+        'moonlight-gray': '#C9C9C9',
+        'dim-white': '#AFAFAF',
+        'luxe-gold': '#D4B483',
+        'shadow-purple': '#1A1113',
+        'muted-red-glow': '#360404ff',
+        
+        // Semantic colors
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+}
